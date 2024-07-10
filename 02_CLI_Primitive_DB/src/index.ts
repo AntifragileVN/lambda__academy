714023b178ee9eb54e39659d239ecf66e5c02dc4
Vitalii Dudar <104485586+AntifragileVN@users.noreply.github.com>
2024-07-10 18:14:58 +0300

    feat: add user search by name
    refactor: migrate user function to separete servce

--- 02_CLI_Primitive_DB/src/index.ts
+++ 02_CLI_Primitive_DB/src/index.ts
@@ -1,51 +1,21 @@
-import { input, select, confirm } from '@inquirer/prompts';
-
-type Gender = 'male' | 'female';
-
-type User = {
-	name: string;
-	gender: Gender;
-	age: number;
-};
+import { confirm } from '@inquirer/prompts';
+import { User } from './types/user.type';
+import { createUser, findUserByName } from './services/user.service';
 
 const USERS: Array<User> = [];
 
-const findUserByName = async () => {
+const run = async () => {
+	await createUser(USERS);
+	if (USERS.length === 0) {
+		console.log('There is no users in database');
+		process.exit(0);
+	}
 	const confirmSearch = await confirm({ message: 'Find a user by name ?' });
 	if (!confirmSearch) {
 		process.exit(0);
 	}
-
-	const searchedUserName = await input({ message: 'Enter searched user name' });
-
-	console.log(USERS);
+	await findUserByName(USERS);
+	process.exit(0);
 };
 
-const createUser = async (): Promise<void> => {
-	const name = await input({ message: 'Enter your name' });
-	if (name === '') {
-		findUserByName();
-		return;
-	}
-	const gender = (await select({
-		message: 'Select gender',
-		choices: [
-			{
-				name: 'male',
-				value: 'male',
-			},
-			{
-				name: 'female',
-				value: 'female',
-			},
-		],
-	})) as Gender;
-
-	const age = await input({ message: 'Enter your age' });
-
-	USERS.push({ name, gender, age: parseInt(age) });
-	createUser();
-	return;
-};
-
-createUser();
+run();
---
+++ 02_CLI_Primitive_DB/src/services/user.service.ts
@@ -0,0 +1,81 @@
+import { input, select, confirm } from '@inquirer/prompts';
+import { isNumeric } from '../helpers/isNumeric.helper';
+import { User, Gender } from '../types/user.type';
+
+export const findUserByName = async (users: Array<User>) => {
+	const searchedUserName = await input({
+		message: 'Enter searched user name',
+		validate: (name: string) => {
+			if (isNumeric(name)) {
+				return 'Name can`t be number';
+			}
+			return true;
+		},
+		transformer: (name) => name.trim(),
+	});
+
+	const foundUser = users.find((user) => user.name === searchedUserName);
+	if (!foundUser) {
+		console.log(`There is no user with name - ${searchedUserName}`);
+	} else {
+		console.log(foundUser);
+	}
+
+	const continueSearch = await confirm({ message: 'Continue users searching ?' });
+	if (!continueSearch) {
+		process.exit(0);
+	}
+	await findUserByName(users);
+	return;
+};
+
+export const createUser = async (USERS: Array<User>): Promise<void> => {
+	const name = await input({
+		message: 'Enter your name',
+		validate: (name: string) => {
+			if (isNumeric(name)) {
+				return 'Name can`t be number';
+			}
+			return true;
+		},
+		transformer: (name) => name.trim(),
+	});
+	if (name === '') {
+		return;
+	}
+
+	const gender = (await select({
+		message: 'Select gender',
+		choices: [
+			{
+				name: 'male',
+				value: 'male',
+			},
+			{
+				name: 'female',
+				value: 'female',
+			},
+		],
+	})) as Gender;
+
+	const age = await input({
+		message: 'Enter your age',
+		validate: (age) => {
+			if (!age) {
+				return 'Age can`t be undefined';
+			}
+			if (!isNumeric(age)) {
+				return 'Age must be a number';
+			}
+			if (parseInt(age) < 0) {
+				return 'Age can`t be negative number';
+			}
+			return true;
+		},
+	});
+
+	USERS.push({ name, gender, age: parseInt(age) });
+	console.log(USERS);
+	await createUser(USERS);
+	return;
+};
