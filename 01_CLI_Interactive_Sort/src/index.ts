ec7acfc1b282ff31c75ee6223a2e2f274e13b90a
Vitaliti Dudar <104485586+AntifragileVN@users.noreply.github.com>
2024-07-11 13:35:10 +0300

    01 feat: start use readline/promises
    refactor: migrate from using recursion to loop

--- 01_CLI_Interactive_Sort/src/index.ts
+++ 01_CLI_Interactive_Sort/src/index.ts
@@ -1,4 +1,4 @@
-import readline from 'readline';
+import readline from 'readline/promises';
 import chalk from 'chalk';
 import {
 	sortAlphabetically,
@@ -7,9 +7,9 @@ import {
 	displayNumbersAscending,
 	displayNumbersDescending,
 	displayWordsAscendingByLength,
-} from './services/sortBy';
-import { checkWordCount, displaySortResult, isNumeric } from './helpers';
-import { errorCharacter, questionCharacter } from './helpers/constants';
+} from './services/sortBy.service';
+import { checkWordCount, displaySortResult, isNumeric } from './helpers/index.helper';
+import { errorCharacter, questionCharacter } from './helpers/constants.helper';
 
 let ENTERED_WORDS = '';
 let REQUIRED_WORD_COUNT = 10;
@@ -39,136 +39,140 @@ const getMenuOptions = () => {
 	return menuOptions;
 };
 
-const askForWords = () => {
-	if (!checkWordCount(ENTERED_WORDS, REQUIRED_WORD_COUNT)) {
-		prompts.question(
+const askForWords = async () => {
+	try {
+		const str = await prompts.question(
 			chalk.bold(
 				`${questionCharacter} Hello. Enter ${REQUIRED_WORD_COUNT} words or digits dividing them in spaces :\n`
-			),
-			(str) => {
-				try {
-					if (!checkWordCount(str.trim(), REQUIRED_WORD_COUNT)) {
-						console.log(
-							`${errorCharacter} There must be exactly ${REQUIRED_WORD_COUNT} words`
-						);
-						askForWords();
-						return;
-					}
-					ENTERED_WORDS = str.trim();
-					askForWords();
-				} catch (error) {
-					console.error(
-						`${errorCharacter} An error occurred while processing your input:`,
-						error
-					);
-					askForWords();
-				}
-			}
+			)
 		);
-	} else {
-		showMenu();
+		if (!checkWordCount(str.trim(), REQUIRED_WORD_COUNT)) {
+			console.log(
+				`${errorCharacter} There must be exactly ${REQUIRED_WORD_COUNT} words`
+			);
+			return '';
+		}
+		return str.trim();
+	} catch (error) {
+		console.error(
+			`${errorCharacter} An error occurred while processing your input:`,
+			error
+		);
+		return '';
 	}
 };
 
-const changeWordsLimit = () => {
-	if (!!REQUIRED_WORD_COUNT) {
-		askForWords();
-		return;
-	} else {
-		prompts.question(
-			chalk.bold(`${questionCharacter} Enter new words count:  `),
-			(str) => {
-				try {
-					const trimmedStr = str.trim();
+const changeWordsLimit = async () => {
+	try {
+		const newWordCount = await prompts.question(
+			chalk.bold(`${questionCharacter} Enter new words count:  `)
+		);
 
-					if (!isNumeric(trimmedStr)) {
-						console.log(`\n${errorCharacter} ${trimmedStr} is not a number`);
-						changeWordsLimit();
-						return;
-					}
-					if (parseInt(trimmedStr) < 2) {
-						console.log(
-							`${errorCharacter} Required words count must be at least 2`
-						);
-						changeWordsLimit();
-						return;
-					}
-					REQUIRED_WORD_COUNT = parseInt(trimmedStr);
-					askForWords();
-				} catch (error) {
-					console.error(
-						`${errorCharacter} An error occurred while processing your input:`,
-						error
-					);
-					changeWordsLimit();
-				}
-			}
+		const trimmedStr = newWordCount.trim();
+
+		if (!isNumeric(trimmedStr)) {
+			console.log(`\n${errorCharacter} ${trimmedStr} is not a number`);
+			changeWordsLimit();
+			return null;
+		}
+		if (parseInt(trimmedStr) < 2) {
+			console.log(`${errorCharacter} Required words count must be at least 2`);
+			changeWordsLimit();
+			return null;
+		}
+		return parseInt(trimmedStr);
+	} catch (error) {
+		console.error(
+			`${errorCharacter} An error occurred while processing your input:`,
+			error
+		);
+		return null;
+	}
+};
+
+const showMenu = async () => {
+	try {
+		const selectedOption = await prompts.question(
+			`\n${questionCharacter} ${chalk.bold(
+				'Please Choose an option:'
+			)}\n\n${getMenuOptions()}`
+		);
+
+		switch (selectedOption.trim()) {
+			case 'a':
+				displaySortResult(ENTERED_WORDS, sortAlphabetically);
+				break;
+			case 'b':
+				displaySortResult(ENTERED_WORDS, displayNumbersAscending);
+				break;
+			case 'c':
+				displaySortResult(ENTERED_WORDS, displayNumbersDescending);
+				break;
+			case 'd':
+				displaySortResult(ENTERED_WORDS, displayWordsAscendingByLength);
+				break;
+			case 'e':
+				displaySortResult(ENTERED_WORDS, showUniqueWords);
+				break;
+			case 'f':
+				displaySortResult(ENTERED_WORDS, showUniqueValueInArray);
+				break;
+			case 'g':
+				ENTERED_WORDS = '';
+				return 'g';
+				break;
+			case 'h':
+				ENTERED_WORDS = '';
+				REQUIRED_WORD_COUNT = 0;
+				return 'h';
+				break;
+			case 'exit':
+				prompts.close();
+				break;
+			default:
+				console.log(`${errorCharacter} No such option. Please enter another: `);
+		}
+	} catch (error) {
+		console.error(
+			`${errorCharacter} An error occurred while processing your selection:`,
+			error
 		);
 	}
 };
 
-const showMenu = () => {
-	prompts.question(
-		`\n${questionCharacter} ${chalk.bold(
-			'Please Choose an option:'
-		)}\n\n${getMenuOptions()}`,
-		(line) => {
-			try {
-				switch (line.trim()) {
-					case 'a':
-						displaySortResult(ENTERED_WORDS, sortAlphabetically);
-						showMenu();
-						break;
-					case 'b':
-						displaySortResult(ENTERED_WORDS, displayNumbersAscending);
-						showMenu();
-						break;
-					case 'c':
-						displaySortResult(ENTERED_WORDS, displayNumbersDescending);
-						showMenu();
-						break;
-					case 'd':
-						displaySortResult(ENTERED_WORDS, displayWordsAscendingByLength);
-						showMenu();
-						break;
-					case 'e':
-						displaySortResult(ENTERED_WORDS, showUniqueWords);
-						showMenu();
-						break;
-					case 'f':
-						displaySortResult(ENTERED_WORDS, showUniqueValueInArray);
-						showMenu();
-						break;
-					case 'g':
-						ENTERED_WORDS = '';
-						askForWords();
-						break;
-					case 'h':
-						ENTERED_WORDS = '';
-						REQUIRED_WORD_COUNT = 0;
-						changeWordsLimit();
-						break;
-					case 'exit':
-						prompts.close();
-						break;
-					default:
-						console.log(
-							`${errorCharacter} No such option. Please enter another: `
-						);
-						showMenu();
-				}
-			} catch (error) {
-				console.error(
-					`${errorCharacter} An error occurred while processing your selection:`,
-					error
-				);
-				showMenu();
+const run = async () => {
+	let continueShowMenu = true;
+	let continueAsking = true;
+	let continueChangeCount = false;
+
+	while (continueShowMenu) {
+		while (continueChangeCount) {
+			const answer = await changeWordsLimit();
+			if (answer) {
+				REQUIRED_WORD_COUNT = answer;
+				continueAsking = true;
+				continueChangeCount = false;
 			}
 		}
-	);
-};
 
-askForWords();
+		while (continueAsking) {
+			const answer = await askForWords();
+			if (answer) {
+				ENTERED_WORDS = answer;
+				continueAsking = false;
+			}
+		}
+
+		const menuResponse = await showMenu();
+		if (menuResponse === 'g') {
+			continueAsking = true;
+		}
+		if (menuResponse === 'h') {
+			continueChangeCount = true;
+		}
+	}
+};
+run();
 
 prompts.on('close', () => {
 	console.log('Exiting the program');
