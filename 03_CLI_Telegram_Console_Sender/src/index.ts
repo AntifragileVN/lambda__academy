895f700df4c3d56516fcac5732f040a4444b8ce9
Vitalii Dudar <104485586+AntifragileVN@users.noreply.github.com>
2024-07-13 13:32:22 +0300

    03 feat: setup TelegramBot
    feat: add message and photo actions
    build: add env.example

--- 03_CLI_Telegram_Console_Sender/src/index.ts
+++ 03_CLI_Telegram_Console_Sender/src/index.ts
@@ -1,24 +1,47 @@
 import { Command } from 'commander';
+import TelegramBot from 'node-telegram-bot-api';
+import 'dotenv/config';
+
 const program = new Command();
+const token = process.env.TELEGRAM_BOT_TOKEN || '';
+const chatId = process.env.USER_CHAT_ID || '';
+
+const bot = new TelegramBot(token, {
+	polling: {
+		autoStart: true,
+		interval: 3000,
+		params: {
+			timeout: 10,
+		},
+	},
+});
 
 program.version('0.0.1');
 
 program
-	.command('m <message>')
-	.alias('message')
+	.command('message <message>')
+	.alias('m')
 	.description('Send message to Telegram Bot')
 	.action((message) => {
-		console.log(message);
+		if (!chatId) {
+			console.log('No chatId available');
+		} else {
+			bot.sendMessage(chatId, message);
+		}
 	});
 
 program
-	.command('p <path>')
-	.alias('photo')
+	.command('photo <path>')
+	.alias('p')
 	.description(
 		'Send photo to Telegram Bot. Just drag and drop it in console after p-flag'
 	)
 	.action((path) => {
-		console.log(path);
+		if (!chatId) {
+			console.log('No chatId available.');
+		} else {
+			bot.sendPhoto(chatId, path);
+		}
 	});
 
 program.parse(process.argv);
