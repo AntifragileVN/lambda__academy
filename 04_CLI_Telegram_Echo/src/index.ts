4ca171aa3d83d8b34818d95a9333929a1f4449fe
Vitalii Dudar <104485586+AntifragileVN@users.noreply.github.com>
2024-07-15 12:39:43 +0300

    04 feat: add message echo and photo sending

--- 04_CLI_Telegram_Echo/src/index.ts
+++ 04_CLI_Telegram_Echo/src/index.ts
@@ -12,3 +12,26 @@ const bot = new TelegramBot(token, {
 		},
 	},
 });
+
+const run = () => {
+	console.log('Telegram bot successfully started. . .');
+};
+
+run();
+
+bot.on('message', (msg) => {
+	try {
+		const userName = msg.from?.first_name;
+		if (msg.text === 'photo') {
+			const dateNow = Date.now();
+			const photoPath = `https://picsum.photos/200?random=${dateNow}`;
+			bot.sendPhoto(msg.chat.id, photoPath);
+			console.log(`Користувач ${userName} запросив картинку`);
+			return;
+		}
+		bot.sendMessage(msg.chat.id, `Ви написали '${msg.text}'`);
+		console.log(`Користувач ${userName} написав:  ${msg.text}`);
+	} catch (error) {
+		console.error(error);
+	}
+});
