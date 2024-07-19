91b709b468b2325c80b3608426f025bbeb8a6669
Vitalii Dudar <104485586+AntifragileVN@users.noreply.github.com>
2024-07-19 18:46:08 +0300

    04 fix: sendMessage trycatch block didn't work

--- 04_CLI_Telegram_Echo/src/index.ts
+++ 04_CLI_Telegram_Echo/src/index.ts
@@ -19,17 +19,17 @@ const run = () => {
 
 run();
 
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
 	try {
 		const userName = msg.from?.first_name;
 		if (msg.text === 'photo') {
 			const dateNow = Date.now();
 			const photoPath = `https://picsum.photos/200?random=${dateNow}`;
-			bot.sendPhoto(msg.chat.id, photoPath);
+			await bot.sendPhoto(msg.chat.id, photoPath);
 			console.log(`Користувач ${userName} запросив картинку`);
 			return;
 		}
-		bot.sendMessage(msg.chat.id, `Ви написали '${msg.text}'`);
+		await bot.sendMessage(msg.chat.id, `Ви написали '${msg.text}'`);
 		console.log(`Користувач ${userName} написав:  ${msg.text}`);
 	} catch (error) {
 		console.error(error);
