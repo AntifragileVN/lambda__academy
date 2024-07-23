d685c53b6722f5a1fa62d3586a9cbb328d70e823
Vitaliti Dudar <104485586+AntifragileVN@users.noreply.github.com>
2024-07-23 17:28:55 +0300

    init task 7

---
+++ 07_CLI_Google_Uploader/src/index.ts
@@ -0,0 +1,51 @@
+import { confirm } from '@inquirer/prompts';
+import { User } from './types/user.type';
+import { createUser, findUserByName } from './services/user.service';
+
+process.stdin.on('data', (key) => {
+	if (key.toString() == '\u0003') {
+		process.exit(0);
+	}
+});
+
+const USERS: Array<User> = [];
+
+const run = async () => {
+	let continueCreating = true;
+	let continueSearching = true;
+
+	while (continueCreating) {
+		const createdUser = await createUser(USERS);
+		console.log(USERS);
+		console.log(`\n`);
+		const confirmContinue = await confirm({ message: 'Create another user ?' });
+		if (!confirmContinue) {
+			continueCreating = false;
+		}
+	}
+
+	const confirmSearch = await confirm({ message: 'Find a user by name ?' });
+	if (!confirmSearch) {
+		console.log('Have a nice day ^-^');
+		process.exit(0);
+	}
+	if (USERS.length === 0) {
+		console.log('There is no users in database\n');
+		process.exit(0);
+	}
+
+	while (continueSearching) {
+		const foundUser = await findUserByName(USERS);
+		if (foundUser) {
+			console.log(foundUser);
+		}
+		const confirmContinue = await confirm({ message: 'Continue users searching ?' });
+		if (!confirmContinue) {
+			continueSearching = false;
+		}
+	}
+	console.log('Have a nice day ^-^');
+	process.exit(0);
+};
+
+run();
---
+++ 07_CLI_Google_Uploader/src/services/user.service.ts
@@ -0,0 +1,79 @@
+import { input, select, confirm } from '@inquirer/prompts';
+import { isNumeric } from '../helpers/isNumeric.helper';
+import { User, Gender } from '../types/user.type';
+
+export const findUserByName = async (users: Array<User>) => {
+	const searchedUserName = await input({
+		message: 'Enter searched user name',
+		validate: (name: string) => {
+			if (name.trim().length <= 0) {
+				return 'Name can`t me empty string';
+			}
+			if (isNumeric(name)) {
+				return 'Name can`t be number';
+			}
+			return true;
+		},
+		transformer: (name) => name.trim(),
+	});
+
+	const foundUser = users.find((user) => user.name === searchedUserName);
+	if (!foundUser) {
+		console.log(`There is no user with name - ${searchedUserName}`);
+		return null;
+	}
+	return foundUser;
+};
+
+export const createUser = async (users: Array<User>) => {
+	const name = await input({
+		message: 'Enter your name',
+		validate: (name: string) => {
+			if (isNumeric(name)) {
+				return 'Name can`t be number';
+			}
+			return true;
+		},
+		transformer: (name) => name.trim(),
+	});
+	if (name === '') {
+		return;
+	}
+
+	const gender = (await select({
+		message: 'Select gender',
+		choices: [
+			{
+				name: 'male',
+				value: 'male',
+			},
+			{
+				name: 'female',
+				value: 'female',
+			},
+		],
+	})) as Gender;
+
+	const age = await input({
+		message: 'Enter your age',
+		validate: (age) => {
+			if (!age) {
+				return 'Age can`t be undefined';
+			}
+			if (!isNumeric(age)) {
+				return 'Age must be a number';
+			}
+			if (parseInt(age) < 0) {
+				return 'Age can`t be negative number';
+			}
+			if (parseInt(age) > 160) {
+				return 'Age can`t be bigger than 160';
+			}
+			return true;
+		},
+	});
+	const createdUser = { name, gender, age: parseInt(age) };
+
+	users.push(createdUser);
+	return createdUser;
+};
